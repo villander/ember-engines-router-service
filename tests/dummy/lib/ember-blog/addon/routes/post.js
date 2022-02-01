aeb1d064736de23bfe9dd353751a5fbde8fce78e
Sergey Astapov <SergeAstapov@gmail.com>
2022-02-01 14:03:08 -0500

    Bump eslint and eslint plugins to latest

--- tests/dummy/lib/ember-blog/addon/routes/post.js
+++ tests/dummy/lib/ember-blog/addon/routes/post.js
@@ -1,29 +1,32 @@
+/* We intentionally test replaceWithExternal method which is not available in controllers */
+/* eslint-disable ember/no-controller-access-in-routes */
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
-export default Route.extend({
+export default class extends Route {
   model(params) {
     return {
       user: this.modelFor('application'),
       id: params.id,
       title: `Post ${params.id}`,
     };
-  },
+  }
 
-  actions: {
-    goToChineseVersion() {
-      this.transitionTo({ queryParams: { lang: 'Chinese' } });
-    },
-    transitionToHome() {
-      this.transitionToExternal('home').then(() => {
-        var postController = this.controllerFor(this.routeName);
-        postController.set('transitionedToExternal', true);
-      });
-    },
-    replaceWithHome() {
-      this.replaceWithExternal('home').then(() => {
-        var postController = this.controllerFor(this.routeName);
-        postController.set('replacedWithExternal', true);
-      });
-    },
-  },
-});
+  @action goToChineseVersion() {
+    this.transitionTo({ queryParams: { lang: 'Chinese' } });
+  }
+
+  @action transitionToHome() {
+    this.transitionToExternal('home').then(() => {
+      var postController = this.controllerFor(this.routeName);
+      postController.set('transitionedToExternal', true);
+    });
+  }
+
+  @action replaceWithHome() {
+    this.replaceWithExternal('home').then(() => {
+      var postController = this.controllerFor(this.routeName);
+      postController.set('replacedWithExternal', true);
+    });
+  }
+}
--- tests/dummy/lib/ember-chat/addon/services/example-service.js
+++ tests/dummy/lib/ember-chat/addon/services/example-service.js
@@ -1,10 +1,12 @@
 import Service, { inject as service } from '@ember/service';
 
-export default Service.extend({
-  store: service(),
-  init() {
-    this._super(...arguments);
+export default class extends Service {
+  @service store;
+
+  constructor () {
+    super(...arguments);
+
     // The store is provided by the containing application, so it's a convenient rendezvous point for our tests to be able to observe this instance.
     this.store.__exampleServiceForTesting = this;
-  },
-});
+  }
+}
