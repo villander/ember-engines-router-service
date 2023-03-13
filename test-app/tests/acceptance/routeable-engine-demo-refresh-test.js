533d2802c2de2a16e2f73c17ae5c211a8e0dc9c8
Alexey Kulakov <uni_que@me.com>
2023-03-13 14:13:43 -0700

    fix: clean up tests

--- test-app/tests/acceptance/routeable-engine-demo-refresh-test.js
+++ test-app/tests/acceptance/routeable-engine-demo-refresh-test.js
@@ -2,44 +2,42 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { visit, find, click } from '@ember/test-helpers';
 
-module('Acceptance | routeless engine demo', function (hooks) {
+module('Acceptance | Engine Router Service | Refresh Method', function (hooks) {
   setupApplicationTest(hooks);
 
-  module('Engine Router Service Refresh Method', function () {
-    test('refresh without params triggers refresh with current route', async function (assert) {
-      await visit('/routable-engine-demo/ember-blog/new');
+  test('refresh without params triggers refresh with current route', async function (assert) {
+    await visit('/routable-engine-demo/ember-blog/new');
 
-      let counter = await find('.route-refresh-counter').textContent;
-      await click('.refresh');
+    let counter = await find('.route-refresh-counter').textContent;
+    await click('.refresh');
 
-      counter = parseInt(counter, 10);
-      counter = ++counter;
-      counter = counter.toString();
-      assert.dom('.route-refresh-counter').hasText(counter);
-    });
+    counter = parseInt(counter, 10);
+    counter = ++counter;
+    counter = counter.toString();
+    assert.dom('.route-refresh-counter').hasText(counter);
+  });
 
-    test('refresh with params triggers refresh on provided route', async function (assert) {
-      await visit('/routable-engine-demo/ember-blog/new');
+  test('refresh with params triggers refresh on provided route', async function (assert) {
+    await visit('/routable-engine-demo/ember-blog/new');
 
-      let counter = await find('.route-refresh-counter').textContent;
-      await click('.refresh-route');
+    let counter = await find('.route-refresh-counter').textContent;
+    await click('.refresh-route');
 
-      counter = parseInt(counter, 10);
-      counter = ++counter;
-      counter = counter.toString();
-      assert.dom('.route-refresh-counter').hasText(counter);
-    });
+    counter = parseInt(counter, 10);
+    counter = ++counter;
+    counter = counter.toString();
+    assert.dom('.route-refresh-counter').hasText(counter);
+  });
 
-    test('refresh external route', async function (assert) {
-      await visit('/routable-engine-demo/ember-blog/new');
+  test('refresh external route', async function (assert) {
+    await visit('/routable-engine-demo/ember-blog/new');
 
-      let counter = await find('.route-refresh-counter').textContent;
-      await click('.refresh-external');
+    let counter = await find('.route-refresh-counter').textContent;
+    await click('.refresh-external');
 
-      counter = parseInt(counter, 10);
-      counter = ++counter;
-      counter = counter.toString();
-      assert.dom('.global-refresh-counter').hasText(counter);
-    });
+    counter = parseInt(counter, 10);
+    counter = ++counter;
+    counter = counter.toString();
+    assert.dom('.global-refresh-counter').hasText(counter);
   });
 });
